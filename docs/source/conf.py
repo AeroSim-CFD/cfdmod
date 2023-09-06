126da6f03285db6fec7fa03d2ccd4fcd20a824e3
Waine Oliveira Jr <waine@aerosim.io>
2023-09-06 11:55:44 -0300

    Initial files commit
    - Added initial structure for docs, module and tests

---
+++ docs/source/conf.py
@@ -0,0 +1,43 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'CFD Mods'
+copyright = '2023, Waine Oliveira Jr, Pablo Penas'
+author = 'Waine Oliveira Jr, Pablo Penas'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.extlinks",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
+    "myst_parser",
+    "nbsphinx",
+]
+
+source_suffix = [".rst", ".md"]
+
+# Include TODOs (check https://www.sphinx-doc.org/en/master/usage/extensions/todo.html)
+todo_include_todos = True
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_book_theme"
+
+html_title = "CFD Mods"
+
+html_static_path = ['_static']
---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+.. CFD Mods documentation master file, created by
+   sphinx-quickstart on Wed Sep  6 11:48:34 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to CFD Mods's documentation!
+====================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
