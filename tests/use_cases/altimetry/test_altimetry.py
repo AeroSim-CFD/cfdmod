f4a5cace80402c863c8a446d359925420b8858e7
pablopenasAerosim <pablo@aerosim.io>
2023-09-13 11:27:27 -0300

    Added probe parsing test

--- tests/use_cases/altimetry/test_altimetry.py
+++ tests/use_cases/altimetry/test_altimetry.py
@@ -6,6 +6,7 @@ import numpy as np
 import trimesh
 
 from cfdmod.use_cases.altimetry import (
+    AltimetryProbe,
     AltimetrySection,
     AltimetryShed,
     plot_altimetry_profiles,
@@ -34,6 +35,45 @@ class TestAltimetryUseCase(unittest.TestCase):
         plot_altimetry_profiles(altimetry_section, output_path)
 
         self.assertEqual(len([x for x in output_path.glob("**/*") if x.is_file()]), 3)
+        temp_dir.cleanup()
+
+    def test_probe_parsing(self):
+        temp_dir = tempfile.TemporaryDirectory()
+        output_path = pathlib.Path(temp_dir.name)
+
+        csv_path = pathlib.Path("./fixtures/tests/probes.csv")
+        surface_mesh: trimesh.Trimesh = trimesh.load_mesh("./fixtures/tests/altimetry/terrain.stl")
+
+        probes = AltimetryProbe.from_csv(csv_path)
+        sections = np.unique([p.section_label for p in probes])
+        altimetry_list: list[AltimetrySection] = []  # For debug plotting purposes
+
+        self.assertEqual(len(sections), 26)
+
+        for sec_label in sections:
+            section_probes = [p for p in probes if p.section_label == sec_label]
+            sheds_in_section = np.unique([p.building_label for p in section_probes])
+            shed_list: list[AltimetryShed] = []
+
+            for shed_label in sheds_in_section:
+                building_probes = [p for p in section_probes if p.building_label == shed_label]
+                shed = AltimetryShed(building_probes[0].coordinate, building_probes[1].coordinate)
+                shed_list.append(shed)
+
+            self.assertEqual(len(shed_list), len(sheds_in_section), 1)
+
+            altimetry_section = AltimetrySection.from_points(
+                sec_label, shed_list[0].start_coordinate, shed_list[0].end_coordinate
+            )
+            altimetry_section.slice_surface(surface_mesh)
+            [altimetry_section.include_shed(s) for s in shed_list]
+
+            plot_altimetry_profiles(altimetry_section, output_path)
+            altimetry_list.append(altimetry_section)
+
+        self.assertEqual(
+            len(altimetry_list), len([x for x in output_path.glob("**/*") if x.is_file()])
+        )
         temp_dir.cleanup()
 
     def test_slicing(self):
