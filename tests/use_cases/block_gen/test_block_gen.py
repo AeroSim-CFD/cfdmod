c6ea5091ebbc121ec20c56f4355b2d758b5622bb
pablopenasAerosim <pablo@aerosim.io>
2023-09-18 10:57:15 -0300

    test block gen

---
+++ tests/use_cases/block_gen/test_block_gen.py
@@ -0,0 +1,50 @@
+import pathlib
+import unittest
+
+from cfdmod.use_cases.block_gen import *
+from cfdmod.use_cases.block_gen import SpacingParams
+
+
+class TestBlockGenerationUseCase(unittest.TestCase):
+    def test_block_generation(self):
+        output_path = pathlib.Path("./output/block_gen")
+
+        block_params = BlockParams(height=5, width=5, length=5)
+        spacing_params = SpacingParams(
+            spacing_x=2, spacing_y=2, line_offset=5, is_abs=False, offset_direction="x"
+        )
+        cfg = GenerationParams(
+            N_blocks_x=10, N_blocks_y=10, block_params=block_params, spacing_params=spacing_params
+        )
+
+        vertices, triangles = build_single_block(cfg.block_params)
+
+        single_line_vertices, single_line_triangles = linear_pattern(
+            vertices,
+            triangles,
+            direction=cfg.spacing_params.offset_direction,
+            n_repeats=cfg.single_line_blocks,
+            spacing_value=cfg.single_line_spacing,
+        )
+
+        full_vertices, full_triangles = linear_pattern(
+            single_line_vertices,
+            single_line_triangles,
+            direction=cfg.perpendicular_direction,
+            n_repeats=cfg.multi_line_blocks,
+            spacing_value=cfg.multi_line_spacing,
+            offset_value=cfg.calculate_spacing(direction=cfg.perpendicular_direction),
+        )
+
+        self.assertEqual(len(vertices), 8)
+        self.assertEqual(len(triangles), 10)
+
+        self.assertEqual(len(single_line_vertices), 8 * cfg.single_line_blocks)
+        self.assertEqual(len(single_line_triangles), 10 * cfg.single_line_blocks)
+
+        self.assertEqual(len(full_vertices), 8 * cfg.single_line_blocks * cfg.multi_line_blocks)
+        self.assertEqual(len(full_triangles), 10 * cfg.single_line_blocks * cfg.multi_line_blocks)
+
+
+if __name__ == "__main__":
+    unittest.main()
