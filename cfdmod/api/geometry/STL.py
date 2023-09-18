96889ceb6f468264f426a85b22908cafd2a19c12
pablopenasAerosim <pablo@aerosim.io>
2023-09-18 15:22:05 -0300

    STL update using the proper geom format

--- cfdmod/api/geometry/STL.py
+++ cfdmod/api/geometry/STL.py
@@ -9,20 +9,22 @@ __all__ = ["export_stl", "read_stl"]
 
 
 def read_stl(filename: pathlib.Path) -> tuple[np.ndarray, np.ndarray]:
-    """Read file content as STL file
+    """Read buffer content as STL file
 
     Args:
-        filename (pathlib.Path): Path of the file to read from
+        buff (io.BufferedReader): buffer to read from
 
     Returns:
-        tuple[np.ndarray, np.ndarray]: return STL representation as (vertices, triangles).
+        tuple[np.ndarray, np.ndarray]: return STL representation as (triangles, normals).
     """
+
+    # pass header
     with open(filename, "rb") as f:
         buff = BytesIO(f.read())
-    # pass header
-    buff.read(80)
 
+    buff.read(80)  # Header
     # Read number of triangles
+
     n_triangles = np.frombuffer(buff.read(4), dtype=np.uint32)[0]
     if n_triangles == 0:
         raise ValueError("Unable to read number of triangles as 0")
@@ -30,53 +32,28 @@ def read_stl(filename: pathlib.Path) -> tuple[np.ndarray, np.ndarray]:
     triangles = np.empty((n_triangles, 3, 3), dtype=np.float32)
     normals = np.empty((n_triangles, 3), dtype=np.float32)
 
-    stl_vertices = np.empty((n_triangles, 3, 3), dtype=np.float32)
-
-    calc_normal = lambda x: np.cross(x[1] - x[0], x[2] - x[0])
-    normalize_func = lambda x: x / np.linalg.norm(x)
-
     for idx in range(n_triangles):
         content = buff.read(50)
-
         normal = np.frombuffer(content[0:12], dtype=np.float32)
-        tri_points = np.frombuffer(content[12:48], dtype=np.float32).reshape((3, 3))
-
-        current_normal = calc_normal(tri_points)
-        current_normal = normalize_func(current_normal)
-
-        if not np.allclose(current_normal, normal):
-            tri_points = np.flip(tri_points)
-
-        stl_vertices[idx] = tri_points
+        triangle = np.frombuffer(content[12:48], dtype=np.float32).reshape((3, 3))
+        triangles[idx] = triangle
         normals[idx] = normal
-        triangles[idx] = tri_points
 
-    stl_vertices = stl_vertices.reshape((n_triangles * 3, 3))
-
-    unique_vertices, indices = np.unique(stl_vertices, axis=0, return_index=True)
-    unique_vertices = stl_vertices[np.sort(indices)]
-    mapped_triangles = np.empty((n_triangles, 3), dtype=np.uint32)
-
-    for i, triangle in enumerate(triangles):
-        mapped_triangles[i] = np.array(
-            [np.where((unique_vertices == vert).all(axis=1)) for vert in triangle]
-        ).reshape(1, 3)[0]
-
-    return unique_vertices, mapped_triangles
+    return triangles, normals
 
 
-def export_stl(filename: pathlib.Path, geom_vertices: np.ndarray, geom_triangles: np.ndarray):
+def export_stl(filename: pathlib.Path, triangle_vertices: np.ndarray, normals: np.ndarray):
     """Export geometry in STL format
 
     Args:
-        filename (pathlib.Path): filename to save to
-        geom_vertices (np.ndarray): Point cloud of the geometry vertices
-        geom_triangles (np.ndarray): List of triangles with geom_vertices indexing
+        filename (pathlib.Path): Filename to save to.
+        triangle_vertices (np.ndarray): Array of the vertices of the triangles.
+        normals (np.ndarray): Array of triangles normals.
     """
     # 80 bytes header
     HEADER = b"\x00" * 80
 
-    n_triangles = len(geom_triangles)
+    n_triangles = len(triangle_vertices)
     # STL content is header + uint32 + 50 bytes per triangle
     stl_content = bytearray(len(HEADER) + 4 + 50 * n_triangles)
 
@@ -86,16 +63,8 @@ def export_stl(filename: pathlib.Path, geom_vertices: np.ndarray, geom_triangles
 
     idx_curr = len(HEADER) + 4
 
-    calc_normal = lambda x: np.cross(
-        geom_vertices[x[1]] - geom_vertices[x[0]], geom_vertices[x[2]] - geom_vertices[x[0]]
-    )
-    normalize_func = lambda x: x / np.linalg.norm(x)
-
-    normals = np.apply_along_axis(calc_normal, axis=1, arr=geom_triangles)
-    normals = np.apply_along_axis(normalize_func, axis=1, arr=normals)
-
-    for idx, tri in enumerate(geom_triangles):
-        p0, p1, p2 = [geom_vertices[tri[0]], geom_vertices[tri[1]], geom_vertices[tri[2]]]
+    for idx, t_verts in enumerate(triangle_vertices):
+        p0, p1, p2 = [t_verts[0], t_verts[1], t_verts[2]]
         normal = normals[idx]
         t_arr = np.array([normal, p0, p1, p2], dtype=np.float32)
 
--- tests/api/geometry/test_STL.py
+++ tests/api/geometry/test_STL.py
@@ -10,17 +10,18 @@ class TestSTL(unittest.TestCase):
     def test_STL_example(self):
         mesh_path = pathlib.Path("./output/api/geometry/test_square.stl")
 
-        single_square_verts = np.array(
-            [[0, 0, 0], [0, 1, 0], [0, 0, 1], [0, 1, 1]], dtype=np.float32
+        single_square_triangles = np.array(
+            [[[0, 0, 0], [0, 1, 0], [0, 0, 1]], [[0, 1, 1], [0, 1, 0], [0, 0, 1]]],
+            dtype=np.float32,
         )
-        single_square_triangles = np.array([[0, 1, 2], [2, 1, 3]], dtype=np.uint32)
+        single_square_normals = np.array([[1, 0, 0], [1, 0, 0]], dtype=np.float32)
 
-        export_stl(mesh_path, single_square_verts, single_square_triangles)
+        export_stl(mesh_path, single_square_triangles, single_square_normals)
 
-        file_verts, file_tri = read_stl(mesh_path)
+        file_triangles, file_normals = read_stl(mesh_path)
 
-        self.assertTrue(all((single_square_verts == file_verts).reshape(1, 12)[0]))
-        self.assertTrue(all((single_square_triangles == file_tri).reshape(1, 6)[0]))
+        self.assertTrue(all((single_square_triangles == file_triangles).reshape(1, 18)[0]))
+        self.assertTrue(all((single_square_normals == file_normals).reshape(1, 6)[0]))
 
 
 if __name__ == "__main__":
