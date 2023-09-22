0e93bc677541ecf60c31d024a008a8e09276265e
Waine Oliveira Jr <waine@aerosim.io>
2023-09-22 13:40:12 -0300

    Corrected __init__ of block gen

--- cfdmod/use_cases/block_gen/__init__.py
+++ cfdmod/use_cases/block_gen/__init__.py
@@ -1,12 +1,3 @@
-from cfdmod.use_cases.block_gen.parameters import (
-    BlockParams,
-    GenerationParams,
-    SpacingParams,
-    OffsetDirection,
-)
-from cfdmod.use_cases.block_gen.build_block import build_single_block
-from cfdmod.use_cases.block_gen.linear_pattern import linear_pattern
-
 __all__ = [
     "BlockParams",
     "GenerationParams",
@@ -15,3 +6,12 @@ __all__ = [
     "build_single_block",
     "linear_pattern",
 ]
+
+from .parameters import (
+    BlockParams,
+    GenerationParams,
+    SpacingParams,
+    OffsetDirection,
+)
+from .build_block import build_single_block
+from .linear_pattern import linear_pattern
