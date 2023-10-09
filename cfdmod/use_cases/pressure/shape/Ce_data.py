6b3f2dcaf35b33a49b1c49c972e2d7ddc57e566f
pablopenasAerosim <harbar2705@gmail.com>
2023-10-09 17:38:28 -0300

    Ce process MVP

--- cfdmod/use_cases/pressure/shape/Ce_data.py
+++ cfdmod/use_cases/pressure/shape/Ce_data.py
@@ -1,6 +1,50 @@
+import numpy as np
 import pandas as pd
+from nassu.lnas import LagrangianGeometry
 
 from cfdmod.use_cases.pressure.statistics import Statistics
+
+
+def transform_to_Ce(
+    surface_mesh: LagrangianGeometry,
+    cp_data: pd.DataFrame,
+    sfc_triangles_idxs: np.ndarray,
+    triangles_region: np.ndarray,
+    n_timesteps: int,
+) -> pd.DataFrame:
+    """Transforms pressure coefficient for surface to shape coefficient
+
+    Args:
+        surface_mesh (LagrangianGeometry): Surface mesh
+        cp_data (pd.DataFrame): Body pressure coefficient data
+        sfc_triangles_idxs (np.ndarray): Surface triangles index from body mesh
+        triangles_region (np.ndarray): Surface triangles region indexing
+        n_timesteps (int): Number of timesteps in data
+
+    Returns:
+        pd.DataFrame: Shape coefficient for surface
+    """
+    triangles_normals = surface_mesh._cross_prod()
+    triangles_areas = np.linalg.norm(triangles_normals, axis=1)
+
+    surface_cp = cp_data[cp_data["point_idx"].isin(sfc_triangles_idxs)].copy()
+
+    surface_cp["region_idx"] = np.tile(triangles_region, n_timesteps)
+    surface_cp["tri_area"] = np.tile(triangles_areas, n_timesteps)
+    surface_cp["f/q"] = surface_cp["cp"] * surface_cp["tri_area"]
+
+    surface_ce = (
+        surface_cp.groupby(["region_idx", "time_step"])  # type: ignore
+        .agg(
+            total_area=pd.NamedAgg(column="tri_area", aggfunc="sum"),
+            total_force=pd.NamedAgg(column="f/q", aggfunc="sum"),
+        )
+        .reset_index()
+    )
+
+    surface_ce["Ce"] = surface_ce["total_force"] / surface_ce["total_area"]
+
+    return surface_ce
 
 
 def calculate_statistics(
@@ -19,6 +63,7 @@ def calculate_statistics(
 
     statistics_data = pd.DataFrame({"region_idx": region_data["region_idx"].unique()})
 
+    # BUG: NaN values
     # if "avg" in statistics_to_apply:
     #     statistics_data["Ce_avg"] = group_by_point["Ce"].mean()
     # if "min" in statistics_to_apply:
