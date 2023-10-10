c0c86746e50137cf7e934e476073ee0e0ea5bef3
pablopenasAerosim <harbar2705@gmail.com>
2023-10-10 18:09:26 -0300

    triangulation still failing

--- cfdmod/use_cases/pressure/shape/region_meshing.py
+++ cfdmod/use_cases/pressure/shape/region_meshing.py
@@ -1,61 +1,122 @@
-from nassu.lnas import LagrangianGeometry
+import pathlib
 
-from cfdmod.use_cases.pressure.shape.meshing_steps import (
-    clean_order_contour_verts,
-    combine_and_sort_full_vertices,
-    define_cutting_points,
-    generate_polyline_edges,
-    get_mesh_contour_polyline,
-    project_grid_intersection_points,
-    triangulate_point_cloud,
-)
+import numpy as np
+from nassu.lnas import LagrangianGeometry
+from scipy.spatial import Delaunay
+
 from cfdmod.use_cases.pressure.shape.regions import ZoningModel
 
 
-def create_regions_mesh(
-    input_mesh: LagrangianGeometry, regions_intervals: ZoningModel, sfc_name
-) -> LagrangianGeometry:
-    """Create a mesh to represent regions projected into an input mesh
+def triangulate_point_cloud(sorted_vertices: np.ndarray) -> np.ndarray:
+    """Creates triangulation for point cloud
 
     Args:
-        input_mesh (LagrangianGeometry): LNAS Mesh object containing mesh points and triangles
-        regions_intervals (ZoningModel): Object to describe regions in x, y and z intervals
+        sorted_vertices (np.ndarray): Array of sorted vertices
 
     Returns:
-        LagrangianGeometry: A new mesh with regions projected into the input mesh
+        np.ndarray: Array of triangulated vertices, with point indices
     """
-    # polyline_vertices, mesh_bbox = get_mesh_contour_polyline(mesh=input_mesh)
-    (polyline_vertices, polyline_edges), mesh_bbox = get_mesh_contour_polyline(mesh=input_mesh)
-
-    polyline_vertices.tofile(f"{sfc_name}.polyline")
-    polyline_edges.tofile(f"{sfc_name}.polyline_edges")
-    ordered_outline_vertices, normal_index = clean_order_contour_verts(
-        mesh_points=input_mesh.vertices,
-        mesh_triangles=input_mesh.triangles,
-        polyline_vertices=polyline_vertices,
+    normal = np.cross(
+        sorted_vertices[1] - sorted_vertices[0], sorted_vertices[2] - sorted_vertices[0]
     )
-    ordered_outline_vertices.tofile(f"{sfc_name}.outline")
+    if np.linalg.norm(normal) == 0:
+        normal = np.cross(
+            sorted_vertices[2] - sorted_vertices[0], sorted_vertices[3] - sorted_vertices[0]
+        )
+    n = normal / np.linalg.norm(normal)
 
-    return input_mesh
+    jitter = 1e-9  # Small random value
+    jittering_array = np.random.uniform(-jitter, jitter, sorted_vertices.shape)
+    sorted_vertices += jittering_array
 
-    # primitive_poly_edges = generate_polyline_edges(
-    #     ordered_outline_vertices=ordered_outline_vertices
-    # )
-    # points_interceptor = define_cutting_points(
-    #     bb_min=mesh_bbox[0],
-    #     bb_max=mesh_bbox[1],
-    #     poly_edges=primitive_poly_edges,
-    #     regions_intervals=regions_intervals,
-    #     ordered_outline_vertices=ordered_outline_vertices,
-    # )
-    # grid_intersection_points = project_grid_intersection_points(points_interceptor)
+    if np.allclose(n, np.array([1, 0, 0])) or np.allclose(n, np.array([-1, 0, 0])):
+        tri = Delaunay(sorted_vertices[:, 1:])
+    elif np.allclose(n, np.array([0, 1, 0])) or np.allclose(n, np.array([0, -1, 0])):
+        tri = Delaunay(sorted_vertices[:, ::2])
+    else:
+        tri = Delaunay(sorted_vertices[:, :2])
 
-    # sorted_vertices = combine_and_sort_full_vertices(
-    #     intersecting_vertices=points_interceptor.get_all_interception_points(),
-    #     ordered_outline_vertices=ordered_outline_vertices,
-    #     grid_intersection_points=grid_intersection_points,
-    #     normal_index=normal_index,
-    # )
-    # triangles = triangulate_point_cloud(sorted_vertices, normal_index)
-    # # return VTPMesh(points=sorted_vertices, triangles=triangles)
-    # return LagrangianGeometry(vertices=sorted_vertices, triangles=triangles)
+    # sorted_vertices -= jittering_array
+
+    return sorted_vertices[tri.simplices]
+
+
+def slice_triangle(tri_verts: np.ndarray, axis: int, axis_value: float):
+    intersected_pts = tri_verts.copy()
+    for i in range(3):
+        if len(intersected_pts) > 4:
+            # Sliced all possible lines
+            continue
+
+        p1, p2 = tri_verts[i], tri_verts[(i + 1) % 3]
+
+        if (p1[axis] < axis_value and p2[axis] > axis_value) or (
+            p1[axis] > axis_value and p2[axis] < axis_value
+        ):
+            t = (axis_value - p1[axis]) / (p2[axis] - p1[axis])
+            intersect_pt = p1 + t * (p2 - p1)
+
+            insert_idx = i + 1 + intersected_pts.shape[0] // 4
+            intersected_pts = np.insert(intersected_pts, insert_idx, intersect_pt, axis=0)
+
+    if len(intersected_pts) == 3:
+        return np.array([tri_verts])
+    else:
+        return triangulate_point_cloud(intersected_pts)
+
+
+def slice_surface(surface: LagrangianGeometry, axis: int, interval: float) -> LagrangianGeometry:
+    new_triangles = np.zeros((0, 3, 3))
+    axis_normal = np.array([0, 0, 0])
+    axis_normal[axis] = 1
+
+    for tri_verts, tri_normal in zip(surface.triangle_vertices, surface.normals):
+        if np.allclose(tri_normal, axis_normal) or np.allclose(tri_normal, -axis_normal):
+            new_triangles = np.concatenate((new_triangles, [tri_verts]), axis=0)
+            continue
+        sliced_triangles = slice_triangle(tri_verts, axis, interval)
+        new_triangles = np.concatenate((new_triangles, sliced_triangles), axis=0)
+
+    full_verts = new_triangles.reshape(len(new_triangles) * 3, 3)
+    verts, triangles = np.unique(full_verts, axis=0, return_inverse=True)
+
+    return LagrangianGeometry(verts, triangles.reshape(-1, 3))
+
+
+def get_mesh_bounds(input_mesh: LagrangianGeometry) -> tuple[tuple[float, float], ...]:
+    x_min, x_max = input_mesh.vertices[:, 0].min(), input_mesh.vertices[:, 0].max()
+    y_min, y_max = input_mesh.vertices[:, 1].min(), input_mesh.vertices[:, 1].max()
+    z_min, z_max = input_mesh.vertices[:, 2].min(), input_mesh.vertices[:, 2].max()
+
+    return ((x_min, x_max), (y_min, y_max), (z_min, z_max))
+
+
+def create_regions_mesh(
+    input_mesh: LagrangianGeometry, regions_intervals: ZoningModel
+) -> LagrangianGeometry:
+    mesh_bounds = get_mesh_bounds(input_mesh)
+    slicing_mesh = input_mesh.copy()
+
+    print(slicing_mesh.triangle_vertices.shape)
+    for x_int in regions_intervals.x_intervals:
+        if x_int <= mesh_bounds[0][0] or x_int >= mesh_bounds[0][1]:
+            continue
+        slicing_mesh = slice_surface(slicing_mesh, 0, x_int)
+
+    print(slicing_mesh.triangle_vertices.shape)
+    for y_int in regions_intervals.y_intervals:
+        if y_int <= mesh_bounds[1][0] or y_int >= mesh_bounds[1][1]:
+            continue
+        slicing_mesh = slice_surface(slicing_mesh, 1, y_int)
+        slicing_mesh.export_stl(pathlib.Path(f"./output/pressure/sfc.y.{y_int}.stl"))
+
+    print(slicing_mesh.triangle_vertices.shape)
+    for z_int in regions_intervals.z_intervals:
+        if z_int <= mesh_bounds[2][0] or z_int >= mesh_bounds[2][1]:
+            continue
+        slicing_mesh = slice_surface(slicing_mesh, 2, z_int)
+        slicing_mesh.export_stl(pathlib.Path(f"./output/pressure/sfc.z.{z_int}.stl"))
+
+    print(slicing_mesh.triangle_vertices.shape)
+
+    return slicing_mesh
