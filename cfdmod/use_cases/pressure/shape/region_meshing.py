d35350585df77791e7d7361760a96240ce56b485
pablopenasAerosim <harbar2705@gmail.com>
2023-10-10 21:15:40 -0300

    Region meshing generalized and working :)

--- cfdmod/use_cases/pressure/shape/region_meshing.py
+++ cfdmod/use_cases/pressure/shape/region_meshing.py
@@ -1,68 +1,70 @@
-import pathlib
-
 import numpy as np
 from nassu.lnas import LagrangianGeometry
-from scipy.spatial import Delaunay
 
 from cfdmod.use_cases.pressure.shape.regions import ZoningModel
 
 
-def triangulate_point_cloud(sorted_vertices: np.ndarray) -> np.ndarray:
-    """Creates triangulation for point cloud
+def triangulate_point_cloud(
+    sorted_vertices: np.ndarray, insertion_indices: list[int]
+) -> np.ndarray:
+    tri_indexes = []
+    if len(insertion_indices) == 1:
+        i = insertion_indices[0]
+        tri_indexes.append([i - 1, i, (i + 2) % 4])
+        tri_indexes.append([i, (i + 1) % 4, (i + 2) % 4])
+    elif len(insertion_indices) == 2:
+        i, j = insertion_indices[0], insertion_indices[1]
+        tri_indexes.append([4, 0, 1])
+        if j == 3:
+            tri_indexes.append([1, 2, 3])
+            tri_indexes.append([3, 4, 1])
+        else:
+            tri_indexes.append([1, 2, 4])
+            tri_indexes.append([2, 3, 4])
 
-    Args:
-        sorted_vertices (np.ndarray): Array of sorted vertices
-
-    Returns:
-        np.ndarray: Array of triangulated vertices, with point indices
-    """
-    normal = np.cross(
-        sorted_vertices[1] - sorted_vertices[0], sorted_vertices[2] - sorted_vertices[0]
-    )
-    if np.linalg.norm(normal) == 0:
-        normal = np.cross(
-            sorted_vertices[2] - sorted_vertices[0], sorted_vertices[3] - sorted_vertices[0]
-        )
-    n = normal / np.linalg.norm(normal)
-
-    jitter = 1e-9  # Small random value
-    jittering_array = np.random.uniform(-jitter, jitter, sorted_vertices.shape)
-    sorted_vertices += jittering_array
-
-    if np.allclose(n, np.array([1, 0, 0])) or np.allclose(n, np.array([-1, 0, 0])):
-        tri = Delaunay(sorted_vertices[:, 1:])
-    elif np.allclose(n, np.array([0, 1, 0])) or np.allclose(n, np.array([0, -1, 0])):
-        tri = Delaunay(sorted_vertices[:, ::2])
+        # if j == 3:
+        #     tri_indexes.append([1, 2, 3])
+        #     tri_indexes.append([3, 4, 1])
+        #     tri_indexes.append([4, 0, 1])
+        # elif i == 2:
+        #     tri_indexes.append([1, 2, 4])
+        #     tri_indexes.append([2, 3, 4])
+        #     tri_indexes.append([4, 0, 1])
+        # else:
+        #     tri_indexes.append([1, 2, 4])
+        #     tri_indexes.append([2, 3, 4])
+        #     tri_indexes.append([4, 0, 1])
     else:
-        tri = Delaunay(sorted_vertices[:, :2])
+        tri_indexes.append([0, 1, 2])
 
-    # sorted_vertices -= jittering_array
-
-    return sorted_vertices[tri.simplices]
+    return sorted_vertices[np.array(tri_indexes)]
 
 
 def slice_triangle(tri_verts: np.ndarray, axis: int, axis_value: float):
     intersected_pts = tri_verts.copy()
+    insertion_indices = []
+
     for i in range(3):
         if len(intersected_pts) > 4:
             # Sliced all possible lines
             continue
+        else:
+            p1, p2 = tri_verts[i], tri_verts[(i + 1) % 3]
 
-        p1, p2 = tri_verts[i], tri_verts[(i + 1) % 3]
+            if (p1[axis] < axis_value and p2[axis] > axis_value) or (
+                p1[axis] > axis_value and p2[axis] < axis_value
+            ):
+                t = (axis_value - p1[axis]) / (p2[axis] - p1[axis])
+                intersect_pt = p1 + t * (p2 - p1)
 
-        if (p1[axis] < axis_value and p2[axis] > axis_value) or (
-            p1[axis] > axis_value and p2[axis] < axis_value
-        ):
-            t = (axis_value - p1[axis]) / (p2[axis] - p1[axis])
-            intersect_pt = p1 + t * (p2 - p1)
-
-            insert_idx = i + 1 + intersected_pts.shape[0] // 4
-            intersected_pts = np.insert(intersected_pts, insert_idx, intersect_pt, axis=0)
+                insert_idx = i + 1 + intersected_pts.shape[0] // 4
+                insertion_indices.append(insert_idx)
+                intersected_pts = np.insert(intersected_pts, insert_idx, intersect_pt, axis=0)
 
     if len(intersected_pts) == 3:
         return np.array([tri_verts])
     else:
-        return triangulate_point_cloud(intersected_pts)
+        return triangulate_point_cloud(intersected_pts, sorted(insertion_indices))
 
 
 def slice_surface(surface: LagrangianGeometry, axis: int, interval: float) -> LagrangianGeometry:
@@ -97,26 +99,19 @@ def create_regions_mesh(
     mesh_bounds = get_mesh_bounds(input_mesh)
     slicing_mesh = input_mesh.copy()
 
-    print(slicing_mesh.triangle_vertices.shape)
     for x_int in regions_intervals.x_intervals:
         if x_int <= mesh_bounds[0][0] or x_int >= mesh_bounds[0][1]:
             continue
         slicing_mesh = slice_surface(slicing_mesh, 0, x_int)
 
-    print(slicing_mesh.triangle_vertices.shape)
     for y_int in regions_intervals.y_intervals:
         if y_int <= mesh_bounds[1][0] or y_int >= mesh_bounds[1][1]:
             continue
         slicing_mesh = slice_surface(slicing_mesh, 1, y_int)
-        slicing_mesh.export_stl(pathlib.Path(f"./output/pressure/sfc.y.{y_int}.stl"))
 
-    print(slicing_mesh.triangle_vertices.shape)
     for z_int in regions_intervals.z_intervals:
         if z_int <= mesh_bounds[2][0] or z_int >= mesh_bounds[2][1]:
             continue
         slicing_mesh = slice_surface(slicing_mesh, 2, z_int)
-        slicing_mesh.export_stl(pathlib.Path(f"./output/pressure/sfc.z.{z_int}.stl"))
-
-    print(slicing_mesh.triangle_vertices.shape)
 
     return slicing_mesh
