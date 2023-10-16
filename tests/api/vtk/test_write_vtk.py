04b473451757565804d5a0517bb0aa9dc39db3e2
pablopenasAerosim <harbar2705@gmail.com>
2023-10-16 10:16:51 -0300

    Tests for shape coefficient

--- tests/api/vtk/test_write_vtk.py
+++ tests/api/vtk/test_write_vtk.py
@@ -1,23 +1,24 @@
 import pathlib
 import unittest
-from unittest.mock import Mock, patch
+
+from vtk import vtkCellArray, vtkPoints, vtkPolyData
 
 from cfdmod.api.vtk.write_vtk import write_polydata
 
 
 class TestWritePolydata(unittest.TestCase):
-    @patch("cfdmod.api.vtk.write_vtk.vtkXMLPolyDataWriter")
-    @patch("cfdmod.api.vtk.write_vtk.create_folders_for_file")
-    def test_write_polydata(self, MockCreateFolders, MockWriter):
+    def test_write_polydata(self):
         output_filename = pathlib.Path("output.vtp")
-        poly_data = Mock()
+        points = vtkPoints()
+        vertices = vtkCellArray()
+        poly_data = vtkPolyData()
+        poly_data.SetPoints(points)
+        poly_data.SetVerts(vertices)
 
         write_polydata(output_filename, poly_data)
 
-        MockCreateFolders.assert_called_with(output_filename)
-        MockWriter().SetFileName.assert_called_with(output_filename.as_posix())
-        MockWriter().SetInputData.assert_called_with(poly_data)
-        MockWriter().Write.assert_called()
+        self.assertTrue(output_filename.exists())
+        output_filename.unlink()
 
 
 if __name__ == "__main__":
