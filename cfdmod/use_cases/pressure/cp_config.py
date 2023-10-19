3fd41e188926a0a66d0f547efc74c1b9829547f8
Waine Oliveira Jr <waine@aerosim.io>
2023-10-19 14:21:27 -0300

    Black + isort

--- cfdmod/use_cases/pressure/cp_config.py
+++ cfdmod/use_cases/pressure/cp_config.py
@@ -9,10 +9,10 @@ from pydantic import BaseModel, Field
 
 from cfdmod.utils import read_yaml
 
-
 __all__ = ["CpConfig", "Statistics"]
 
 Statistics = Literal["max", "min", "std", "avg", "skewness", "kurtosis"]
+
 
 class CpConfig(BaseModel):
     timestep_range: tuple[float, float] = Field(
