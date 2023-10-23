42c3a7faf3b19bb2dff4cccb789764852e09ca46
pablopenasAerosim <harbar2705@gmail.com>
2023-10-23 10:59:38 -0300

    tests for Cm

---
+++ tests/use_cases/pressure/moment/test_Cm_main.py
@@ -0,0 +1,17 @@
+import unittest
+
+from cfdmod.use_cases.pressure.moment.main import main
+
+
+class TestCeMain(unittest.TestCase):
+    def test_main(self):
+        output = "./output/pressure"
+        cp = "./fixtures/tests/pressure/data/cp_t.resampled.hdf"
+        config = "./fixtures/tests/pressure/Cm_params.yaml"
+        mesh = "./fixtures/tests/pressure/galpao/galpao.normalized.lnas"
+
+        main(["--output", output, "--cp", cp, "--config", config, "--mesh", mesh])
+
+
+if __name__ == "__main__":
+    unittest.main()
