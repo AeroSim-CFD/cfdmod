d7c18e3fbc552b3ee27531139d94a1d888bcc84f
pablopenasAerosim <harbar2705@gmail.com>
2023-11-20 16:06:36 -0300

    loft functions refactor

--- cfdmod/use_cases/loft/functions.py
+++ cfdmod/use_cases/loft/functions.py
@@ -1,21 +1,20 @@
 import math
-from typing import Tuple
 
 import numpy as np
 
 
-def find_border(vertices: np.ndarray) -> set:
-    """Extract vertices and unique edges for the stl file's border in x-y plane
+def find_border(triangle_vertices: np.ndarray) -> tuple[np.ndarray, set]:
+    """Extract vertices from unique edges from stl file's border with its triangles and normals
 
     Args:
-        stl_file (STLFile): STL file containing terrain mesh.
+        triangle_vertices (np.ndarray): STL triangles
 
     Returns:
-        Tuple[np.ndarray, set]: Tuple containing the mesh's vertices and its border edges vertices
+        tuple[np.ndarray, set]: Vertices from the border and a set with border edges
     """
-    s = vertices.shape
+    s = triangle_vertices.shape
 
-    flattened_vertices = vertices.reshape((s[0] * s[1], 3))
+    flattened_vertices = triangle_vertices.reshape((s[0] * s[1], 3))
 
     # Round for comparison
     decimals = 0
@@ -28,16 +27,16 @@ def find_border(vertices: np.ndarray) -> set:
     # Indexed as [t_idx, edge_idx] = (v0, v1)
     tri_index_matrix = np.empty((s[0], 3, 2), dtype=np.uint32)
 
-    for t_idx, tri in enumerate(vertices):
+    for t_idx, tri in enumerate(triangle_vertices):
         v_idxs = []
-        for v in (tri.v0, tri.v1, tri.v2):
+        for v in tri:
             key = get_as_key(v)
             val = flat_indexes[key]
             v_idxs.append(val)
         tri_edges = [tuple(sorted((v_idxs[i], v_idxs[j]))) for i, j in [(0, 1), (1, 2), (2, 0)]]
         tri_index_matrix[t_idx] = tri_edges
 
-    n_triangles = len(triangles)
+    n_triangles = len(triangle_vertices)
     flat_edges = tri_index_matrix.reshape(n_triangles * 3, 2)
     flat_edges_tp = [tuple(edge) for edge in flat_edges]
 
@@ -49,77 +48,90 @@ def find_border(vertices: np.ndarray) -> set:
             unique_edges.add(edge)
         elif edge in unique_edges:
             unique_edges.remove(edge)
-
-    return flattened_vertices, unique_edges
-
-
-def project_border(
-    unique_edges: set,
-    flattened_vertices: np.ndarray,
-    flow_angle: int,
-    center: Tuple[float, float, float],
-    size: Tuple[float, float],
-):
-    """_summary_
-
-    Args:
-        unique_edges (set): _description_
-        flattened_vertices (np.ndarray): _description_
-        flow_angle (int): _description_
-        center (Tuple[float, float, float]): _description_
-        size (Tuple[float, float]): _description_
-    """
-
-    def get_vector_from_points(
-        p0: Tuple[float, float, float], p1: Tuple[float, float, float]
-    ) -> Tuple[float, float]:
-        """Return a tuple with a vector from p0 to p1 in the x-y plane"""
-        return (p1[0] - p0[0], p1[1] - p0[1])
-
-    def get_angle_between(ref_vec, target_vec):
-        """Returns the angle in radians between vectors 'ref_vec' and 'target_vec'"""
-
-        def unit_vector(vector):
-            """Returns the unit vector of the vector."""
-            return vector / np.linalg.norm(vector)
-
-        ref_vec_u = unit_vector(ref_vec)
-        target_vec_u = unit_vector(target_vec)
-
-        angle = np.rad2deg(np.arccos(np.dot(ref_vec_u, target_vec_u)))
-        dot = (
-            ref_vec_u[0] * target_vec_u[0] + ref_vec_u[1] * target_vec_u[1]
-        )  # Dot product between [x1, y1] and [x2, y2]
-        det = ref_vec_u[0] * target_vec_u[1] - ref_vec_u[1] * target_vec_u[0]  # Determinant
-        angle = -np.rad2deg(np.arctan2(det, dot))  # atan2(y, x) or atan2(sin, cos)
-
-        return angle + 360 if angle < 0 else angle
-
-    projection_angle = math.radians(flow_angle)
-
     border_indexes = [point_index for edge in unique_edges for point_index in edge]
 
-    p0 = (
-        center[0] - size[0] * math.sin(projection_angle) / 2,
-        center[1] + size[1] * math.cos(projection_angle) / 2,
+    return flattened_vertices[np.unique(border_indexes)], unique_edges
+
+
+def get_angle_between(ref_vec: np.ndarray, target_vec: np.ndarray) -> float:
+    """Returns the angle in radians between vectors 'ref_vec' and 'target_vec'
+
+    Args:
+        ref_vec (np.ndarray): Reference vector
+        target_vec (np.ndarray): Target vector
+
+    Returns:
+        float: Angle between vectors 'ref_vec' and 'target_vec in degrees
+    """
+
+    def unit_vector(vector):
+        """Returns the unit vector of the vector."""
+        return vector / np.linalg.norm(vector)
+
+    ref_vec_u = unit_vector(ref_vec)
+    target_vec_u = unit_vector(target_vec)
+    cross_prod = np.cross(ref_vec_u, target_vec_u)
+
+    angle_rad = np.arctan2(np.linalg.norm(cross_prod), np.dot(ref_vec_u, target_vec_u))
+    angle = np.degrees(angle_rad)
+    if cross_prod[2] < 0:
+        angle = 360 - angle
+
+    return angle + 360 if angle < 0 else angle
+
+
+def project_border(border_vertices: np.ndarray, projection_diretion: np.ndarray) -> np.ndarray:
+    """Projects the border vertices based on wind source direction
+
+    Args:
+        border_vertices (np.ndarray): Border vertices
+        projection_diretion (np.ndarray): Direction which to project the border
+
+    Returns:
+        np.ndarray: Ordered border profile vertices
+    """
+    flow_angle = get_angle_between(
+        ref_vec=(1, 0, 0),
+        target_vec=projection_diretion,
     )
-    p1 = (
-        center[0] + size[0] * math.sin(projection_angle) / 2,
-        center[1] - size[1] * math.cos(projection_angle) / 2,
+    projection_angle = math.radians(flow_angle)
+    size = (
+        border_vertices[:, 0].max() - border_vertices[:, 0].min(),
+        border_vertices[:, 1].max() - border_vertices[:, 1].min(),
+    )
+    center = np.array(
+        [
+            (border_vertices[:, 0].max() + border_vertices[:, 0].min()) / 2,
+            (border_vertices[:, 1].max() + border_vertices[:, 1].min()) / 2,
+            (border_vertices[:, 2].max() + border_vertices[:, 2].min()) / 2,
+        ]
     )
 
-    separation_line = get_vector_from_points(p0, p1)
+    p0 = np.array(
+        [
+            center[0] - size[0] * math.sin(projection_angle) / 2,
+            center[1] + size[1] * math.cos(projection_angle) / 2,
+            (border_vertices[:, 2].max() + border_vertices[:, 2].min()) / 2,
+        ]
+    )
+    p1 = np.array(
+        [
+            center[0] + size[0] * math.sin(projection_angle) / 2,
+            center[1] - size[1] * math.cos(projection_angle) / 2,
+            (border_vertices[:, 2].max() + border_vertices[:, 2].min()) / 2,
+        ]
+    )
+
+    separation_line = p1 - p0
 
     profile_vertices = np.empty((0, 3))
 
-    for p_index in np.unique(border_indexes):
-        target_point = flattened_vertices[p_index]
-        if np.cross(separation_line, get_vector_from_points(p1, target_point)) >= 0:
+    for target_point in border_vertices:
+        target_vec = target_point - p1
+        if np.cross(separation_line[:2], target_vec[:2]) >= 0:
             profile_vertices = np.vstack((profile_vertices, target_point))
 
-    theta_sort = lambda x: get_angle_between(
-        ref_vec=get_vector_from_points(center, p0), target_vec=get_vector_from_points(center, x)
-    )
+    theta_sort = lambda x: get_angle_between(ref_vec=p0 - center, target_vec=x - center)
 
     profile_vertices = np.array(sorted(profile_vertices, key=theta_sort))
 
---
+++ tests/use_cases/loft/test_functions.py
@@ -0,0 +1,54 @@
+import unittest
+
+import matplotlib.tri as tri
+import numpy as np
+
+from cfdmod.use_cases.loft.functions import find_border, get_angle_between, project_border
+
+
+class TestLoftFunctions(unittest.TestCase):
+    def setUp(self):
+        self.nx, self.ny = 3, 3
+        x = np.linspace(-10, 10, self.nx + 1)
+        y = np.linspace(-10, 10, self.ny + 1)
+        z = np.array([1])
+        xv, yv, zv = np.meshgrid(x, y, z)
+
+        vertices = np.vstack((xv.flatten(), yv.flatten(), zv.flatten())).T
+        triangles = tri.Triangulation(vertices[:, 0], vertices[:, 1]).triangles
+
+        self.triangle_vertices = vertices[triangles]
+        super().setUp()
+
+    def test_find_border(self):
+        border_verts, border_edges = find_border(triangle_vertices=self.triangle_vertices)
+        expected_edge_count = (self.nx + self.ny) * 2
+        expected_vertex_count = (self.nx + self.ny + 2) * 2 - 4
+        self.assertEqual(len(border_edges), expected_edge_count)
+        self.assertEqual(len(border_verts), expected_vertex_count)
+
+    def test_angle_between(self):
+        vec1 = np.array([1, 0, 0])
+        vec2 = np.array([0, 1, 0])
+        vec3 = np.array([1, 1, 0])
+        vec4 = np.array([-1, -1, 0])
+        self.assertEqual(get_angle_between(vec1, vec2), 90)
+        self.assertEqual(get_angle_between(vec1, vec3), 45)
+        self.assertEqual(get_angle_between(vec1, vec4), 225)
+        self.assertEqual(get_angle_between(vec2, vec3), 315)
+        self.assertEqual(get_angle_between(vec3, vec4), 180)
+
+    def test_project_border(self):
+        border_verts, _ = find_border(triangle_vertices=self.triangle_vertices)
+        border_profile = project_border(border_verts, projection_diretion=np.array([1, 0, 0]))
+        self.assertTrue(all(border_profile[:, 0] >= 0))
+        border_profile = project_border(border_verts, projection_diretion=np.array([-1, 0, 0]))
+        self.assertTrue(all(border_profile[:, 0] <= 0))
+        border_profile = project_border(border_verts, projection_diretion=np.array([0, 1, 0]))
+        self.assertTrue(all(border_profile[:, 1] >= 0))
+        border_profile = project_border(border_verts, projection_diretion=np.array([0, -1, 0]))
+        self.assertTrue(all(border_profile[:, 1] <= 0))
+
+
+if __name__ == "__main__":
+    unittest.main()
