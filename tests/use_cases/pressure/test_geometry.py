51b4607418a05903774d35c8ee82c42e93793454
pabloharbar <harbar2705@gmail.com>
2024-01-18 10:18:04 -0300

    added more tests for geometry

--- tests/use_cases/pressure/test_geometry.py
+++ tests/use_cases/pressure/test_geometry.py
@@ -2,8 +2,14 @@ import unittest
 
 import numpy as np
 from lnas import LnasFormat, LnasGeometry
+from vtk import vtkPolyData
 
-from cfdmod.use_cases.pressure.geometry import get_excluded_surfaces
+from cfdmod.use_cases.pressure.geometry import (
+    combine_geometries,
+    create_NaN_polydata,
+    filter_geometry_from_list,
+    get_excluded_surfaces,
+)
 
 
 class TestGeometry(unittest.TestCase):
@@ -39,6 +45,45 @@ class TestGeometry(unittest.TestCase):
         with self.assertRaises(Exception) as context:
             get_excluded_surfaces(self.mesh, sfc_list)
         self.assertEqual(str(context.exception), "Surface is not defined in LNAS.")
+
+    def test_create_NaN_polydata(self):
+        column_labels = ["col1", "col2"]
+        result_polydata = create_NaN_polydata(self.mesh.geometry, column_labels)
+
+        cell_data = result_polydata.GetCellData()
+        num_cell_arrays = cell_data.GetNumberOfArrays()
+        cell_scalar_names = [cell_data.GetArrayName(i) for i in range(num_cell_arrays)]
+
+        self.assertIsInstance(result_polydata, vtkPolyData)
+        self.assertTrue(all(column in cell_scalar_names for column in column_labels))
+
+    def test_filter_geometry_from_list(self):
+        surface_list = ["sfc1"]
+
+        result_geometry, result_triangle_idxs = filter_geometry_from_list(self.mesh, surface_list)
+
+        self.assertIsInstance(result_geometry, LnasGeometry)
+        self.assertIsInstance(result_triangle_idxs, np.ndarray)
+
+        self.assertTrue(result_triangle_idxs == np.array([0]))
+
+    def test_combine_geometries(self):
+        geometry_list = [
+            LnasGeometry(
+                vertices=np.array([[0, 0, 0], [1, 0, 0], [0, 1, 0]]),
+                triangles=np.array([[0, 1, 2]]),
+            ),
+            LnasGeometry(
+                vertices=np.array([[1, 0, 0], [0, 1, 0], [1, 1, 0]]),
+                triangles=np.array([[0, 1, 2]]),
+            ),
+        ]
+
+        result_geometry = combine_geometries(geometry_list)
+
+        self.assertIsInstance(result_geometry, LnasGeometry)
+        self.assertTrue((result_geometry.triangles == np.array([[0, 1, 2], [3, 4, 5]])).all())
+        self.assertTrue(len(result_geometry.vertices) == 6)
 
 
 if __name__ == "__main__":
