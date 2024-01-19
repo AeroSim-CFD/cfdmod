267c9b3ef78c05c2c6091aeec0bbcfd812bc1a2b
pabloharbar <harbar2705@gmail.com>
2024-01-19 15:41:03 -0300

    profiling for pressure use cases

---
+++ tests/use_cases/pressure/profiling/Ce_profiling.py
@@ -0,0 +1,17 @@
+from memory_profiler import profile
+
+from cfdmod.use_cases.pressure.shape.main import main as main_Ce
+
+
+@profile
+def main_Ce_profiling():
+    output = "./output/profiling"
+    cp = "./fixtures/tests/pressure/data/cp_t.grouped.h5"
+    config = "./fixtures/tests/pressure/Ce_params.yaml"
+    mesh = "./fixtures/tests/pressure/galpao/galpao.normalized.lnas"
+
+    main_Ce(["--output", output, "--cp", cp, "--config", config, "--mesh", mesh])
+
+
+if __name__ == "__main__":
+    main_Ce_profiling()
---
+++ tests/use_cases/pressure/profiling/Cf_profiling.py
@@ -0,0 +1,17 @@
+from memory_profiler import profile
+
+from cfdmod.use_cases.pressure.force.main import main as main_Cf
+
+
+@profile
+def main_Cf_profiling():
+    output = "./output/profiling"
+    cp = "./fixtures/tests/pressure/data/cp_t.grouped.h5"
+    config = "./fixtures/tests/pressure/Cf_params.yaml"
+    mesh = "./fixtures/tests/pressure/galpao/galpao.normalized.lnas"
+
+    main_Cf(["--output", output, "--cp", cp, "--config", config, "--mesh", mesh])
+
+
+if __name__ == "__main__":
+    main_Cf_profiling()
