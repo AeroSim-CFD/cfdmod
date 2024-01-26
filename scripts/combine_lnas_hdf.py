900589b364b4df372d56a536cfe38ab150d58efc
pabloharbar <harbar2705@gmail.com>
2024-01-26 16:37:52 -0300

    validate args

--- scripts/combine_lnas_hdf.py
+++ scripts/combine_lnas_hdf.py
@@ -17,6 +17,8 @@ class ArgsModel:
     lnas: str
     hdf: str
     output: str
+    start_time: float
+    end_time: float
 
 
 def get_args_process(args: list[str]) -> ArgsModel:
@@ -30,22 +32,39 @@ def get_args_process(args: list[str]) -> ArgsModel:
     """
     ap = argparse.ArgumentParser()
     ap.add_argument(
+        "-l",
         "--lnas",
         required=True,
         help="Path of lnas file",
         type=str,
     )
     ap.add_argument(
+        "-hdf",
         "--hdf",
         required=True,
         help="Path of hdf time series data",
         type=str,
     )
     ap.add_argument(
+        "-o",
         "--output",
         required=True,
         help="Output path for generated files",
         type=str,
+    )
+    ap.add_argument(
+        "-st",
+        "--start-time",
+        help="Temporal range start time",
+        default=0,
+        type=float,
+    )
+    ap.add_argument(
+        "-et",
+        "--end-time",
+        help="Temporal range end time",
+        default=float("inf"),
+        type=float,
     )
 
     parsed_args = ap.parse_args(args)
@@ -56,6 +75,9 @@ def get_args_process(args: list[str]) -> ArgsModel:
 def main(*args):
     args_use = get_args_process(*args)
 
+    if args_use.end_time <= args_use.start_time:
+        raise ValueError("The value for the end time must be bigger than the start time")
+
     lnas_path = pathlib.Path(args_use.lnas)
     hdf_path = pathlib.Path(args_use.hdf)
     output_path = pathlib.Path(args_use.output)
@@ -63,7 +85,14 @@ def main(*args):
     mesh = LnasFormat.from_file(lnas_path)
     data = pd.read_hdf(hdf_path)
 
+    if data.time_step.min() > args_use.end_time or data.time_step.max() < args_use.start_time:
+        raise Exception("The temporal range does not include any time steps in the data")
+
     variables = [col for col in data.columns if col not in ["time_step", "point_idx"]]
+
+    data = data.loc[
+        (data["time_step"] >= args_use.start_time) & (data["time_step"] <= args_use.end_time)
+    ]
 
     stats = calculate_statistics(
         historical_data=data,
@@ -72,6 +101,8 @@ def main(*args):
         group_by_key="point_idx",
     )
     polydata = create_polydata_for_cell_data(data=stats, mesh=mesh.geometry)
+
+    print(args_use)
 
     write_polydata(output_filename=output_path, poly_data=polydata)
 
