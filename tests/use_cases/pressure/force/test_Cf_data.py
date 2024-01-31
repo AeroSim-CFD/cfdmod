c6bdeaec61e05b6985e09ae19d1a262ec322eff7
pabloharbar <harbar2705@gmail.com>
2024-01-31 14:24:45 -0300

    added test of liquid force

--- tests/use_cases/pressure/force/test_Cf_data.py
+++ tests/use_cases/pressure/force/test_Cf_data.py
@@ -2,11 +2,12 @@ import unittest
 
 import numpy as np
 import pandas as pd
-from lnas import LnasGeometry
+from lnas import LnasFormat, LnasGeometry
 
 from cfdmod.api.geometry.transformation_config import TransformationConfig
 from cfdmod.use_cases.pressure.force.Cf_data import get_representative_areas, transform_Cf
 from cfdmod.use_cases.pressure.geometry import GeometryData, tabulate_geometry_data
+from cfdmod.use_cases.pressure.zoning.processing import calculate_statistics
 from cfdmod.use_cases.pressure.zoning.zoning_model import ZoningModel
 
 
@@ -49,3 +50,45 @@ class TestCfData(unittest.TestCase):
             len(cf_data), self.cp_data.time_step.nunique() * geometry_df.region_idx.nunique()
         )  # Three timesteps x 1 region
         self.assertTrue(all([f"Cf{var}" in cf_data.columns for var in ["x", "y", "z"]]))
+
+    def test_liquid_coefficients(self):
+        cp_data = pd.DataFrame(
+            {
+                "point_idx": [0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3],
+                "cp": [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2],
+                "time_step": [0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2],
+            }
+        )
+
+        upper_mesh = LnasFormat(
+            version="", geometry=self.body_geom, surfaces={"sfc1": np.array([0, 1])}
+        )
+        lower_mesh = upper_mesh.copy()
+        geometry_negative = lower_mesh.geometry
+        geometry_negative.triangles = np.flip(geometry_negative.triangles, 1)
+        geometry_negative._full_update()
+        geometry_negative.vertices = (
+            geometry_negative.vertices + geometry_negative.vertices_normals * 0.1
+        )
+        upper_mesh.join(lnas_fmts=[lower_mesh], surfaces_suffixes=["_lower"])
+        geom_data = GeometryData(
+            mesh=upper_mesh.geometry,
+            zoning_to_use=ZoningModel(),
+            triangles_idxs=np.array([0, 1, 2, 3]),
+        )
+        geometry_dict = {"body": geom_data}
+
+        geometry_df = tabulate_geometry_data(
+            geom_dict=geometry_dict,
+            mesh_areas=upper_mesh.geometry.areas,
+            mesh_normals=upper_mesh.geometry.normals,
+            transformation=TransformationConfig(),
+        )
+        cf_data = transform_Cf(cp_data, geometry_df, upper_mesh.geometry)
+        cf_stats = calculate_statistics(
+            historical_data=cf_data,
+            statistics_to_apply=["mean"],
+            variables=["Cfz"],
+            group_by_key="region_idx",
+        )
+        self.assertEqual(round(cf_stats.iloc[0].Cfz_mean, 1), 0.6)
