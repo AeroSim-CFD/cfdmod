d8408e9e537bbf012b51441fff9f2bba12a83cc0
Waine Oliveira Jr <waine@aerosim.io>
2024-02-23 14:50:45 -0300

    Removed memory profiler

--- tests/use_cases/pressure/profiling/cp_profiling.py
+++ tests/use_cases/pressure/profiling/cp_profiling.py
@@ -1,9 +1,10 @@
-from memory_profiler import profile
-
 from cfdmod.use_cases.pressure.main import main as main_cp
 
 
-@profile
+# Install memory-profiler (pip install -U memory-profiler) and run this as 
+# 
+# mprof run poetry run python tests/use_cases/pressure/profiling/cp_profiling.py
+# mprof plot
 def main_cp_profiling():
     output = "./output/profiling"
     p = "./fixtures/tests/pressure/data/bodies.galpao.data.grouped.h5"
