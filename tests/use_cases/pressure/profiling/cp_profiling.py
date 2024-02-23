6d98950c449e2e282a11db284c12c97ac07eab77
Waine Oliveira Jr <waine@aerosim.io>
2024-02-23 14:59:18 -0300

    Minor update

--- tests/use_cases/pressure/profiling/cp_profiling.py
+++ tests/use_cases/pressure/profiling/cp_profiling.py
@@ -2,8 +2,9 @@ from cfdmod.use_cases.pressure.main import main as main_cp
 
 
 # Install memory-profiler (pip install -U memory-profiler) and run this as 
-# 
-# mprof run poetry run python tests/use_cases/pressure/profiling/cp_profiling.py
+#
+# poetry shell
+# mprof run python tests/use_cases/pressure/profiling/cp_profiling.py
 # mprof plot
 def main_cp_profiling():
     output = "./output/profiling"
