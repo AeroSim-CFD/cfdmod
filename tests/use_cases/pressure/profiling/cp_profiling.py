745db61b06a8e26d8343c9aa7f942d16035a994b
pabloharbar <harbar2705@gmail.com>
2024-02-23 16:28:41 -0300

    Updates to profiling instructions

--- tests/use_cases/pressure/profiling/cp_profiling.py
+++ tests/use_cases/pressure/profiling/cp_profiling.py
@@ -1,11 +1,6 @@
 from cfdmod.use_cases.pressure.main import main as main_cp
 
 
-# Install memory-profiler (pip install -U memory-profiler) and run this as 
-#
-# poetry shell
-# mprof run python tests/use_cases/pressure/profiling/cp_profiling.py
-# mprof plot
 def main_cp_profiling():
     output = "./output/profiling"
     p = "./fixtures/tests/pressure/data/bodies.galpao.data.grouped.h5"
