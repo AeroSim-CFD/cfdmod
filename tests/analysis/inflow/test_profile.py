897566cabde0d0e1b5ee45534de1977e9ef7c84f
pabloharbar <harbar2705@gmail.com>
2024-02-27 10:15:35 -0300

    tests for inflow

--- tests/analysis/inflow/test_profile.py
+++ tests/analysis/inflow/test_profile.py
@@ -6,35 +6,48 @@ from cfdmod.analysis.inflow.profile import InflowData, NormalizationParameters
 
 class TestInflowProfile(unittest.TestCase):
     def setUp(self):
+        self.inflow_data = {}
         folder_path = pathlib.Path("./fixtures/tests/inflow/pitot_inlet")
-        self.inflow_data = InflowData.from_folder(folder_path)
+        self.inflow_data["old_version"] = InflowData.from_files(
+            hist_series_path=folder_path / "hist_series.csv",
+            points_path=folder_path / "points.csv",
+        )
+        folder_path = pathlib.Path("./fixtures/tests/inflow/autocorrelacao_10_h5")
+        self.inflow_data["new_version"] = InflowData.from_files(
+            hist_series_path=folder_path / "hist_series.h5",
+            points_path=folder_path / "points.csv",
+        )
         super().setUp()
 
     def test_calculate_mean_velocity(self):
-        result = self.inflow_data.calculate_mean_velocity(for_components=["ux", "uy", "uz"])
-        self.assertTrue(all([f"{c}_mean" in result.columns for c in ["ux", "uy", "uz"]]))
+        for inflow_data in self.inflow_data.values():
+            result = inflow_data.calculate_mean_velocity(for_components=["ux", "uy", "uz"])
+            self.assertTrue(all([f"{c}_mean" in result.columns for c in ["ux", "uy", "uz"]]))
 
     def test_calculate_turbulence_intensity(self):
-        result = self.inflow_data.calculate_turbulence_intensity(for_components=["ux", "uy", "uz"])
-        self.assertTrue(all([f"I_{c}" in result.columns for c in ["ux", "uy", "uz"]]))
+        for inflow_data in self.inflow_data.values():
+            result = inflow_data.calculate_turbulence_intensity(for_components=["ux", "uy", "uz"])
+            self.assertTrue(all([f"I_{c}" in result.columns for c in ["ux", "uy", "uz"]]))
 
     def test_calculate_spectral_density(self):
         normalization_params = NormalizationParameters(
             reference_velocity=1.0, characteristic_length=1.0
         )
-        result = self.inflow_data.calculate_spectral_density(
-            target_index=0,
-            for_components=["ux", "uy", "uz"],
-            normalization_params=normalization_params,
-        )
-        self.assertTrue(all([f"S ({c})" in result.columns for c in ["ux", "uy", "uz"]]))
-        self.assertTrue(all([f"f ({c})" in result.columns for c in ["ux", "uy", "uz"]]))
+        for inflow_data in self.inflow_data.values():
+            result = inflow_data.calculate_spectral_density(
+                target_index=0,
+                for_components=["ux", "uy", "uz"],
+                normalization_params=normalization_params,
+            )
+            self.assertTrue(all([f"S ({c})" in result.columns for c in ["ux", "uy", "uz"]]))
+            self.assertTrue(all([f"f ({c})" in result.columns for c in ["ux", "uy", "uz"]]))
 
     def test_calculate_autocorrelation(self):
-        result = self.inflow_data.calculate_autocorrelation(
-            anchor_point_idx=0, for_components=["ux", "uy", "uz"]
-        )
-        self.assertTrue(all([f"coef_{c}" in result.columns for c in ["ux", "uy", "uz"]]))
+        for inflow_data in self.inflow_data.values():
+            result = inflow_data.calculate_autocorrelation(
+                anchor_point_idx=0, for_components=["ux", "uy", "uz"]
+            )
+            self.assertTrue(all([f"coef_{c}" in result.columns for c in ["ux", "uy", "uz"]]))
 
 
 if __name__ == "__main__":
