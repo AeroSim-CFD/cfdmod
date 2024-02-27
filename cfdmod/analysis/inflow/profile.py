2709d63bbc219e9877b618555777b5a5b60696b7
pabloharbar <harbar2705@gmail.com>
2024-02-27 10:14:24 -0300

    update profile file format

--- cfdmod/analysis/inflow/profile.py
+++ cfdmod/analysis/inflow/profile.py
@@ -146,7 +146,22 @@ class InflowData:
         return autocorrelation
 
     @classmethod
-    def from_folder(cls, probe_name: str, folder_path: pathlib.Path) -> InflowData:
-        data = pd.read_csv(folder_path / f"{probe_name}.data.h5")
-        points = pd.read_csv(folder_path / f"{probe_name}.points.csv")
+    def from_files(cls, hist_series_path: pathlib.Path, points_path: pathlib.Path) -> InflowData:
+        hist_series_format = hist_series_path.name.split(".")[-1]
+        if hist_series_format == "csv":
+            data = pd.read_csv(hist_series_path)
+        elif hist_series_format == "h5":
+            data_dfs = []
+            with pd.HDFStore(hist_series_path, mode="r") as data_store:
+                for key in data_store.keys():
+                    df = data_store.get(key)
+                    data_dfs.append(df)
+
+            data = pd.concat(data_dfs)
+            data.sort_values(
+                by=[col for col in ["time_step", "point_idx"] if col in data.columns], inplace=True
+            )
+        else:
+            raise Exception(f"Extension {hist_series_format} not supported for hist series!")
+        points = pd.read_csv(points_path)
         return InflowData(data=data, points=points)
