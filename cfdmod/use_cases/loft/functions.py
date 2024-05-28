ab3e89fcfe364250917704571e560c2dad9f176b
aronZavelinski <aron@aerosim.io>
2024-05-27 22:57:29 -0300

    pymeshlab function parameter correction

--- cfdmod/use_cases/loft/functions.py
+++ cfdmod/use_cases/loft/functions.py
@@ -286,7 +286,7 @@ def apply_remeshing(element_size: float, mesh_path: pathlib.Path, output_path: p
     """
     ms: MeshSet = pymeshlab.MeshSet()
     ms.load_new_mesh(str(mesh_path.absolute()))
-    ms.meshing_isotropic_explicit_remeshing(iterations=15, targetlen=AbsoluteValue(element_size), crease_angle: float)
+    ms.meshing_isotropic_explicit_remeshing(iterations=15, targetlen=AbsoluteValue(element_size), featuredeg=crease_angle)
     ms.save_current_mesh(str(output_path.absolute()), binary=True)
 
 
