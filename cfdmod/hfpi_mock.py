5353409cdd87c5ed4952f9862ab551ec28890901
Waine Oliveira Jr <waine@aerosim.io>
2025-06-13 12:29:12 -0300

    Updated HFPI plots

--- cfdmod/hfpi_mock.py
+++ cfdmod/hfpi_mock.py
@@ -9,6 +9,7 @@ import numpy as np
 import scipy
 from scipy.ndimage import gaussian_filter
 from scipy import integrate
+import matplotlib.pyplot as plt
 
 
 def _validate_keys_df(df: pd.DataFrame, keys: list[str]):
@@ -141,7 +142,7 @@ class HFPIStructuralData(BaseModel):
         ]
         self.is_normalized = True
 
-
+ 
 class WindAnalysis(BaseModel):
     """Data for wind analysis and calculation"""
     model_config = ConfigDict(arbitrary_types_allowed=True)
@@ -371,8 +372,8 @@ def solve_runge_kunta(gen_force: np.ndarray, dt: float, wp: float, xi: float) ->
         dvdt = -2 * xi * wp * v - x + F_t
         return [dxdt, dvdt]
 
-    x0 = gen_force[0]
-    df = (gen_force[1] - gen_force[0]) / dt
+    x0 = gen_force.mean() / (wp ** 2)
+    df = (gen_force[1:] - gen_force[:-1]).mean() / dt
     v0 = df / (2 * xi * wp) if xi * wp != 0 else 0.0
 
     sol = integrate.solve_ivp(system, (t_eval[0], t_eval[-1]), [x0, v0], t_eval=t_eval, method='RK45')
@@ -496,14 +497,14 @@ class HFPISolver(BaseModel):
         return total_displacement
 
 
-def plot_force_series(
+def plot_force_spectrum(
     forces_data: HFPIForcesData,
     structure_data: HFPIStructuralData,
     *,
     plot_mz: bool = True,
     sigma: float = 2,
 ):
-    fig, axs = plt.subplots(1, 2, figsize=(12, 4))
+    fig, ax = plt.subplots(1, 1, figsize=(6, 4))
     dt = forces_data.delta_t
     df_mode = structure_data.df_modes
     coef_style = {"FX": "blue", "FY": "red", "MZ": "grey"}
@@ -517,61 +518,73 @@ def plot_force_series(
             continue
         global_force = df_force.drop(columns=["time"]).sum(axis=1)
 
-        axs[0].plot(df_force["time"], global_force, color=coef_style[f_name], label=f_name)
-        axs[0].set_ylabel("F [N ou N.m]")
-        axs[0].set_xlabel("t [s]")
-        axs[0].legend(loc="lower left", frameon=False)
-
         (freq, PSD) = scipy.signal.periodogram(global_force, 1 / dt, scaling="density")
         PSD = PSD * freq / (np.std(global_force) ** 2)
-        axs[1].loglog(
-            freq, gaussian_filter(PSD, sigma=sigma), color=coef_style[f_name], label=f_name
+        ax.loglog(
+            freq, gaussian_filter(PSD, sigma=sigma), color=coef_style[f_name], label=f_name, alpha=0.8,
         )
-        axs[1].loglog([df_mode["frequency"], df_mode["frequency"]], [1e-5, 1e1], color="black")
-        axs[1].set_ylim([1e-5, 1e1])
+        ax.loglog([df_mode["frequency"], df_mode["frequency"]], [1e-5, 1e1], color="black")
+        ax.set_ylim(1e-4, 1e1)
+        ax.set_xlim(1e-3, 3)
 
-        axs[1].set_ylabel(r"$ S(F) f / \tilde{F}^2 $")
-        axs[1].set_xlabel("f [Hz]")
-        axs[1].legend(loc="lower left", frameon=False)
+        ax.set_ylabel(r"$ S(F) f / \tilde{F}^2 $")
+        ax.set_xlabel("f [Hz]")
+        ax.legend(loc="lower left", frameon=False)
 
-    return fig, axs
+    return fig, ax
 
-def plot_force_series(
-    forces_data: HFPIForcesData,
-    structure_data: HFPIStructuralData,
+def plot_displacement(
+    displacement_dict: dict[str, np.ndarray],
     *,
-    plot_mz: bool = True,
-    sigma: float = 2,
+    floor_plot: int,
+    plot_limit: float,
+    start_step_idx: int = 0,
 ):
-    fig, axs = plt.subplots(1, 2, figsize=(12, 4))
-    dt = forces_data.delta_t
-    df_mode = structure_data.df_modes
-    coef_style = {"FX": "blue", "FY": "red", "MZ": "grey"}
+    fig, ax = plt.subplots(1, 1, figsize=(6, 4))
 
-    for f_name, df_force in [
-        ("FX", forces_data.cf_x),
-        ("FY", forces_data.cf_y),
-        ("MZ", forces_data.cm_z),
-    ]:
-        if not plot_mz and f_name == "MZ":
-            continue
-        global_force = df_force.drop(columns=["time"]).sum(axis=1)
+    x_disp = displacement_dict["x"][start_step_idx:, floor_plot]
+    y_disp = displacement_dict["y"][start_step_idx:, floor_plot]
+    x_avg = x_disp.mean()
+    y_avg = y_disp.mean()
 
-        axs[0].plot(df_force["time"], global_force, color=coef_style[f_name], label=f_name)
-        axs[0].set_ylabel("F [N ou N.m]")
-        axs[0].set_xlabel("t [s]")
-        axs[0].legend(loc="lower left", frameon=False)
+    ax.set_xlim(-plot_limit+x_avg, plot_limit+x_avg)
+    ax.set_ylim(-plot_limit+y_avg, plot_limit+y_avg)
+    ax.set_ylabel("y [m]")
+    ax.set_xlabel("x [m]")
 
-        (freq, PSD) = scipy.signal.periodogram(global_force, 1 / dt, scaling="density")
-        PSD = PSD * freq / (np.std(global_force) ** 2)
-        axs[1].loglog(
-            freq, gaussian_filter(PSD, sigma=sigma), color=coef_style[f_name], label=f_name
+    # Use differente alphas for plot
+    n_samples = len(displacement_dict["x"])
+    n_bins = 80
+    max_alpha = 0.9
+    min_alpha = 0.1
+    bin_size = n_samples // 80
+
+    for n_bin in range(n_bins):
+        alpha = min_alpha + n_bin / n_bins * (max_alpha-min_alpha)
+        start = bin_size * n_bin
+        end = bin_size * (n_bin + 1) if n_bin != bin_size -1 else -1
+
+        ax.plot(
+            x_disp[start : end],
+            y_disp[start : end],
+            color="b",
+            alpha=alpha,
         )
-        axs[1].loglog([df_mode["frequency"], df_mode["frequency"]], [1e-5, 1e1], color="black")
-        axs[1].set_ylim([1e-5, 1e1])
 
-        axs[1].set_ylabel(r"$ S(F) f / \tilde{F}^2 $")
-        axs[1].set_xlabel("f [Hz]")
-        axs[1].legend(loc="lower left", frameon=False)
+    return fig, ax
 
-    return fig, axs
+def set_plt_style():
+    plt.style.use("seaborn-v0_8-whitegrid")
+    plt.rcParams["font.family"] = "Ubuntu"
+    plt.rcParams["font.size"] = 10
+    plt.rcParams["mathtext.fontset"] = "custom"
+    plt.rcParams["legend.facecolor"] = "white"
+    plt.rcParams["legend.edgecolor"] = "none"
+    plt.rcParams["xtick.direction"] = "in"
+    plt.rcParams["ytick.direction"] = "in"
+    plt.rcParams["lines.linestyle"] = "-"
+    plt.rcParams["lines.linewidth"] = 2
+    plt.rcParams["lines.markersize"] = 6
+    plt.rcParams["lines.markeredgecolor"] = "none"
+    plt.rcParams["axes.edgecolor"] = "black"
+    plt.rcParams["figure.edgecolor"] = "red"
